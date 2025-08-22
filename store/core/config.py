20d513f15256f622a5a87a85424f21f08d633163
ThayOlive <thl.oliver.1999@gmail.com>
2025-08-22 12:36:17 -0300

    config finais

--- store/core/config.py
+++ store/core/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Store API"
-    ROOT_PATH: str = "/"    
-    DATABASE_URL: str 
-    
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8")
+    ROOT_PATH: str = "/"
+
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
